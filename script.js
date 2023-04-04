322acba8e724d75166d53395da1b22d5c8dd1e83
Jai Kushwaha <kushwahajai74>
2023-04-05 03:21:02 +0530

    added some files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
